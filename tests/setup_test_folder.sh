557f316a0d1e4da2fcf144fdbcb2c6cf77a27824
Daniel Sauvé <megametres@users.noreply.github.com>
2023-10-26 18:49:31 -0400

    Create a script to generate files and folders to manually test

---
+++ tests/setup_test_folder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Simple file to generates files and folder to test
+
+mkdir -p sample_path/sample_parent_a/sample_child_a1
+mkdir -p sample_path/sample_parent_a/sample_child_a2
+mkdir -p sample_path/sample_parent_b/sample_child_b1
+mkdir -p sample_path/sample_parent_c/sample_child_c1
+
+touch sample_path/sample_parent_a/sample_child_a1/sample_file_a1
+touch sample_path/sample_parent_a/sample_child_a2/sample_file_a2
+touch sample_path/sample_parent_b/sample_child_b1/sample_file_b
+touch sample_path/sample_parent_c/sample_child_c1/sample_file_c
