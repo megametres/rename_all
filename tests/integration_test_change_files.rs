d84476caab85fb481b99ea6e30663d8921c168a2
Daniel Sauvé <megametres@users.noreply.github.com>
2023-11-08 20:32:26 -0500

    Add test rename content different cases

--- tests/integration_test_change_files.rs
+++ tests/integration_test_change_files.rs
@@ -19,3 +19,57 @@ fn test_rename_file() {
     assert_eq!(file_content, "test");
     temp_file.close().unwrap();
 }
+
+#[test]
+fn test_rename_file_lowercase() {
+    let temp_file = prepare_tmpdir();
+    temp_file.child("file").write_str("sample").unwrap();
+
+    let mut cmd = prepare_cmd();
+    cmd.arg("--lowercase")
+        .arg("SAMPLE")
+        .arg("TEST")
+        .arg(temp_file.path())
+        .assert()
+        .success();
+
+    let file_content: String = read_file_content(&temp_file.child("file"));
+    assert_eq!(file_content, "test");
+    temp_file.close().unwrap();
+}
+
+#[test]
+fn test_rename_file_uppercase() {
+    let temp_file = prepare_tmpdir();
+    temp_file.child("file").write_str("SAMPLE").unwrap();
+
+    let mut cmd = prepare_cmd();
+    cmd.arg("--uppercase")
+        .arg("sample")
+        .arg("test")
+        .arg(temp_file.path())
+        .assert()
+        .success();
+
+    let file_content: String = read_file_content(&temp_file.child("file"));
+    assert_eq!(file_content, "TEST");
+    temp_file.close().unwrap();
+}
+
+#[test]
+fn test_rename_file_capitalize() {
+    let temp_file = prepare_tmpdir();
+    temp_file.child("file").write_str("Sample").unwrap();
+
+    let mut cmd = prepare_cmd();
+    cmd.arg("--capitalize")
+        .arg("sample")
+        .arg("test")
+        .arg(temp_file.path())
+        .assert()
+        .success();
+
+    let file_content: String = read_file_content(&temp_file.child("file"));
+    assert_eq!(file_content, "Test");
+    temp_file.close().unwrap();
+}
